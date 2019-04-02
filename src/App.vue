4838f8435b5cfc562de6888e7279beabc84aecf7
Jake Garcia <bballwiz4@gmail.com>
2019-04-02 13:43:03 -0700

    initial commit

---
+++ src/App.vue
@@ -0,0 +1,12 @@
+<template>
+  <div id="app">
+    <nav class="navbar navbar-dark bg-dark">
+    <span class="navbar-brand mb-0 h1">Jake's ToDo App</span>
+  </nav>
+    <router-view/>
+  </div>
+</template>
+
+<style>
+
+</style>
